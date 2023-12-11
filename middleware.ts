e571dd90f9534e9f078a72913f4ef5bbe3bad22b
MicaelTargino <micaelgomestargino@gmail.com>
2023-12-10 21:31:20 -0300

    init of organization implementation

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,23 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
  
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-    publicRoutes:["/"]
+    publicRoutes:["/"],
+    afterAuth(auth, req) {
+      if (auth.userId && auth.isPublicRoute) {
+        let path = '/select-org';
+
+        if (auth.orgId) {
+          path = `/organization/${auth.orgId}`
+        };
+
+        const orgSelection = new URL(path, req.url);
+        return NextResponse.redirect(orgSelection);
+      }
+      
+    }
 });
- 
+
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
