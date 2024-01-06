709b4ff5430e752491f0c4c751956dcf499c0ce0
MicaelTargino <micaelgomestargino@gmail.com>
2024-01-06 13:48:18 -0300

    add form popover to nav btns

--- app/(platform)/(dashboard)/_components/NavBar.tsx
+++ app/(platform)/(dashboard)/_components/NavBar.tsx
@@ -2,6 +2,7 @@ import { Plus } from "lucide-react"
 import { Logo } from "@/components/logo"
 import { Button } from "@/components/ui/button"
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs"
+import { FormPopover } from "@/components/form/form-popover"
 import { MobileSidebar } from "./MobileSidebar"
 
 export const Navbar = () => {
@@ -12,12 +13,16 @@ export const Navbar = () => {
                 <div className="hidden md:flex">
                     <Logo />
                 </div>
-                <Button variant="primary" size="sm" className="rounded-sm hidden md:block h-auto py-1.5 px-2">
-                    Create
-                </Button>
-                <Button variant="primary" size="sm" className="rounded-sm block md:hidden h-auto">
-                    <Plus className="h-4 w-4" />
-                </Button>
+                <FormPopover align="start" side="bottom" sideOffset={18}>
+                    <Button variant="primary" size="sm" className="rounded-sm hidden md:block h-auto py-1.5 px-2">
+                        Create
+                    </Button>
+                </FormPopover>
+                <FormPopover>
+                    <Button variant="primary" size="sm" className="rounded-sm block md:hidden h-auto">
+                        <Plus className="h-4 w-4" />
+                    </Button>
+                </FormPopover>
             </div>
             <div className="ml-auto flex items-center gap-x-2">
                 <OrganizationSwitcher 
