b4a2988bb263c91f3aff77664b8410822de3f85a
MicaelTargino <micaelgomestargino@gmail.com>
2024-01-07 15:36:59 -0300

    create deleteboard action

--- app/(platform)/(dashboard)/board/[boardId]/_components/BoardNavbar.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/BoardNavbar.tsx
@@ -3,6 +3,7 @@ import { auth } from "@clerk/nextjs";
 import { Board } from "@prisma/client";
 import { redirect } from "next/navigation";
 import { BoardTitleForm } from "./BoardTitleForm";
+import { BoardOptions } from "./BoardOptions";
 
 
 interface BoardNavbarProps {
@@ -13,6 +14,9 @@ export const BoardNavbar = async ({data}: BoardNavbarProps) => {
     return (
         <div className="w-full h-14 z-[40] bg-black/50 fixed top-14 flex items-center px-6 gap-x-4 text-white">
             <BoardTitleForm data={data} />
+            <div className="ml-auto">
+                <BoardOptions id={data.id} />
+            </div>
         </div>
     )
 };
