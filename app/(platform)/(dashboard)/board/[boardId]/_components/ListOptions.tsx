5694caf409266c9c587e61b194bd45f6d557dde9
MicaelTargino <micaelgomestargino@gmail.com>
2024-01-10 12:46:35 -0300

    fix: force popover close after list deletion

--- app/(platform)/(dashboard)/board/[boardId]/_components/ListOptions.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/ListOptions.tsx
@@ -7,6 +7,10 @@ import { Button } from "@/components/ui/button";
 import { MoreHorizontal, X } from "lucide-react";
 import { FormSubmit } from "@/components/form/form-submit";
 import { Separator } from "@/components/ui/separator";
+import { useAction } from "@/hooks/use-action";
+import { deleteList } from "@/actions/delete-list";
+import { toast } from "sonner";
+import { ElementRef, useRef } from "react";
 
 interface ListOptionsProps {
     data: List;
@@ -14,6 +18,22 @@ interface ListOptionsProps {
 }
 
 export const ListOptions = ({data, onAddCard}: ListOptionsProps) => {
+    const closeRef = useRef<ElementRef<"button">>(null);
+
+    const {execute: executeDelete} = useAction(deleteList, {
+        onSuccess: (data) => {
+            toast.success(`List ${data.title} deleted`);
+            closeRef.current?.click();
+        },
+        onError: (err) => toast.error(err)
+    })
+
+    const onDelete = (formData:FormData) => {
+        const id = formData.get('id') as string;
+        const boardId = formData.get('boardId') as string;
+
+        executeDelete({id, boardId})
+    }
     return (
         <Popover>
             <PopoverTrigger asChild>
@@ -25,7 +45,7 @@ export const ListOptions = ({data, onAddCard}: ListOptionsProps) => {
             <div className="pb-4 text-sm font-medium text-center text-neutral-600">
                 List Actions
             </div>
-            <PopoverClose asChild>
+            <PopoverClose ref={closeRef}  asChild>
                 <Button className="w-auto h-auto p-2 absolute top-2 right-2 text-neutral-600" variant="ghost">
                     <X className="w-4 h-4" />
                 </Button>
@@ -41,7 +61,7 @@ export const ListOptions = ({data, onAddCard}: ListOptionsProps) => {
                 </FormSubmit>
             </form>
             <Separator />
-            <form>
+            <form action={onDelete}>
                 <input hidden name="id" id="id" value={data.id} />
                 <input hidden name="boardId" id="boardId" value={data.boardId} />
                 <FormSubmit variant="ghost" className="rounded-none w-full h-auto p-2 px-5 justify-start font-normal text-neutral-600">
