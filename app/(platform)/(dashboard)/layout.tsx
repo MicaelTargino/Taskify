b63431e4e734cfdf1491b016e9da7ea679538187
MicaelTargino <micaelgomestargino@gmail.com>
2024-02-05 22:31:13 -0300

    refactor pomodoro code

--- app/(platform)/(dashboard)/layout.tsx
+++ app/(platform)/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import { Navbar } from "./_components/NavBar";
+import { PomodoroTriggerButton } from "./board/[boardId]/_components/PomodoroTriggerButton";
 const DashboradLayout = ({children}: {children: React.ReactNode})  => {
     return (
         <div className="h-full">
--- app/(platform)/(dashboard)/pomodoro/_components/Pomodoro.tsx
+++ app/(platform)/(dashboard)/pomodoro/_components/Pomodoro.tsx
@@ -1,249 +1,151 @@
 "use client"
 
-import { History, Pause, Play, Settings, SkipBack, SkipForward, TimerReset } from "lucide-react";
-import {useState, useRef, useEffect} from "react";
-import { cn } from "@/lib/utils";
-import { useSearchParams } from 'next/navigation'
- 
+import { useState, useRef, useEffect, MutableRefObject } from 'react';
+import { Pause, Play, SkipBack, SkipForward } from 'lucide-react';
 
+const DEFAULT_WORK_DURATION = 45 * 60; // 45 minutes in seconds
+const DEFAULT_BREAK_DURATION = 15 * 60; // 15 minutes in seconds
 
-const Pomodoro = ({url}: {url:string}) => {
-    const [workDuration, setWorkDuration] = useState<number>(45 * 60); // 25 minutes in seconds
-    const [breakDuration, setBreakDuration] = useState<number>(15 * 60); // 5 minutes in seconds
-    const [timeLeft, setTimeLeft] = useState<number>(workDuration);
-    const [isWorking, setIsWorking] = useState<boolean>(true); // Flag to track work/break state
-    const [isRunning, setIsRunning] = useState<boolean>(false);
-    const [rounds, setRounds] = useState(0);
-    const [percentage, setPercentage] = useState(0);
-    const timerRef = useRef<number | null>(null);
-    const [modalViewable, setModalViewable] = useState(false);
-    const [settingsModalViewable, setSettingsModalViewable] = useState(false);
-    const [tickMute, setTickMute] = useState(false);
-    const [alertMute, setAlertMute] = useState(false);
+// Define a type that works for both the browser and Node.js environments
+type TimeoutRef =  number | ReturnType<typeof setTimeout> | null;
 
-   
-    const searchParams = useSearchParams();
-    const imgurl = searchParams.get('imgurl');
-
-    const handleTickMute = (mute: boolean) => {
-        setTickMute(mute);
-    } 
-
-    const handleAlertMute = (mute: boolean) => {
-        setAlertMute(mute);
-    } 
+const Pomodoro = ({ url }: {url: string}) => {
+  const [duration, setDuration] = useState({ work: DEFAULT_WORK_DURATION, break: DEFAULT_BREAK_DURATION });
+  const [timeLeft, setTimeLeft] = useState(duration.work);
+  const [isWorking, setIsWorking] = useState(true);
+  const [isRunning, setIsRunning] = useState(false);
+  const [rounds, setRounds] = useState(0);
+  const timerRef: MutableRefObject<TimeoutRef> = useRef<TimeoutRef>(null);
 
     useEffect(() => {
+      // Initial reset to work duration
         resetTimer();
     }, []);
 
-    
-    function playAudio(type: string) {
-    let audio;
-    switch (type) {
-      case 'tick':
-         if (tickMute) return false;
-         audio = new Audio('/static_audio_tick.mp3');
-        break;
-      case 'done':
-        if (alertMute) return false;
-        audio = new Audio('/alert-long-break.mp3');
-        break
-    }
-    audio?.play();
-  }
+    useEffect(() => {
+        // Handle timeLeft reaching zero
+        if (timeLeft <= 0) {
+            toggleWorkBreak();
+            playAudio('done');
+        }
+    }, [timeLeft]);
 
-
-  const finish = () => {
-    playAudio('done');
-    setWorkDuration(45 * 60);
-    setBreakDuration(45 * 15);
-    setTimeLeft(45 * 60);
-    setIsWorking(true);
-    setIsRunning(false);
-    setRounds(0);
-    setPercentage(0);
-    setModalViewable(true);
-    if (timerRef.current) {
-      clearInterval(timerRef.current);
+    // Define a type guard for the Timeout object specific to Node.js
+    function isTimeout(value: TimeoutRef): value is NodeJS.Timeout {
+      return typeof value === 'object' && value !== null;
     }
-  }
 
-  const addRound = async () => {
-    const url = process.env.NEXT_PUBLIC_BACKEND_API_URL || 'default';
-    // const roundCat = workDuration == (45 * 60) ? 0 : 1
-    // const res = url != 'default' ? await axios.post(url + `api/pomodoro/addPomodoroRound/${user?.id}/` + Math.floor(workDuration / 60), {}, {
-    //   headers: {
-    //     Authorization: Authorization
-    //   }
-    // }) : {data: 'seta a env pow'}
-    // const data = res.data;
-    // if (data.status_code == 200) {
-    setRounds(rounds + 1)
-    // } else {
-    //   console.log('error in saving rounds')
-    }
+    const toggleWorkBreak = () => {
+        setIsWorking(!isWorking);
+        setTimeLeft(isWorking ? duration.break : duration.work);
+        if (isWorking) setRounds(rounds + 1);
+        setIsRunning(false);
+        if (timerRef.current !== null) {
+          if (typeof timerRef.current === 'number' || isTimeout(timerRef.current)) {
+            clearInterval(timerRef.current as any);
+          }
+        }
+    };
+
+    const playAudio = (type: string) => {
+        const audioUrl = type == 'tick' ? '/static_audio_tick.mp3' : '/alert-long-break.mp3';
+        const audio = new Audio(audioUrl);
+        audio.play();
+    };
+
+    const startTimer = () => {
+        setIsRunning(true);
+        if (timerRef.current !== null) {
+          if (typeof timerRef.current === 'number' || isTimeout(timerRef.current)) {
+            clearInterval(timerRef.current as any);
+          }
+        }
+        timerRef.current = setInterval(() => {
+            setTimeLeft((prev) => Math.max(prev - 1, 0));
+        }, 1000);
+    };
+
+    const pauseTimer = () => {
+        setIsRunning(false);
+        if (timerRef.current !== null) {
+          if (typeof timerRef.current === 'number' || isTimeout(timerRef.current)) {
+            clearInterval(timerRef.current as any);
+          }
+        }
+    };
+
+    const resetTimer = () => {
+        setTimeLeft(isWorking ? duration.work : duration.break);
+        if (timerRef.current !== null) {
+          if (typeof timerRef.current === 'number' || isTimeout(timerRef.current)) {
+            clearInterval(timerRef.current as any);
+          }
+        }
+    };
+
+    const changePomodoroCategory = (workTime:number, breakTime:number) : void => {
+        setDuration({ work: workTime * 60, break: breakTime * 60 });
+        setTimeLeft(workTime * 60);
+        setIsWorking(true);
+        setIsRunning(false);
+        if (timerRef.current !== null) {
+          if (typeof timerRef.current === 'number' || isTimeout(timerRef.current)) {
+            clearInterval(timerRef.current as any);
+          }
+        }
+    };
   
-
   const jumpRound = () => {
-    playAudio('done');
     setTimeLeft(0);
     playAudio('done')
     if (isWorking) {
-    //   addRound();
-      setRounds(rounds)
+      setRounds((rounds) => rounds++)
     }
-  }
-
-  useEffect(() => {
-    if (timeLeft > 0) {
-      const percentage = isWorking ? Math.ceil(((workDuration - timeLeft) / workDuration) * 100) : Math.ceil(((breakDuration - timeLeft) / breakDuration) * 100);
-      setPercentage(percentage);
-    } else {
-      playAudio('done');
-      if (isWorking) {
-        addRound()
-        // setRounds(rounds + 1)
-        setIsWorking(false);
-        setTimeLeft(breakDuration);
-      } else {
-        setIsWorking(true);
-        setTimeLeft(workDuration);
-      }
-
-      if (timerRef.current) {
-        clearInterval(timerRef.current);
-      }
-      setIsRunning(false);
-    }
-  }, [timeLeft])
-
-  const startTimer = () => {
-    setIsRunning(true);
-
-    if (timerRef.current) {
-      clearInterval(timerRef.current);
-    }
-
-    timerRef.current = window.setInterval(() => {
-      setTimeLeft((prevTimeLeft) => {
-        if (prevTimeLeft >= 1) {
-          // const percentage = isWorking ? (workDuration - timeLeft) / workDuration : (breakDuration - timeLeft) / breakDuration
-          // console.log(workDuration - timeLeft)
-          // setPercentage(percentage)
-          // const percentage = isWorking ? Math.ceil(((workDuration - timeLeft) / workDuration) * 100) : Math.ceil(((breakDuration - timeLeft) / breakDuration) * 100);
-          // setPercentage(percentage);
-          if (prevTimeLeft <= 5 ) playAudio('tick');
-          return prevTimeLeft - 1;
-        } else {
-          playAudio('done')
-          if (isWorking) {
-            addRound();
-            // setRounds(rounds + 1)
-            setIsWorking(false);
-            setTimeLeft(breakDuration);
-          } else {
-            setIsWorking(true);
-            setTimeLeft(workDuration);
-          }
-
-          if (timerRef.current) {
-            clearInterval(timerRef.current);
-          }
-          setIsRunning(false);
-          return prevTimeLeft;
-        }
-      });
-    }, 1000);
-  };
-
-  const pauseTimer = () => {
-    setIsRunning(false);
-    if (timerRef.current) {
-      clearInterval(timerRef.current);
-    }
-  };
-
-  const resetTimer = () => {
-    // setIsWorking(true);
-    if (isWorking == true) {
-      setTimeLeft(workDuration);
-    } else {
-      setTimeLeft(breakDuration)
-    }
-    if (timerRef.current) {
-      clearInterval(timerRef.current);
-    }
-    // startTimer();
-  };
-
-  function closeModal() {
-    setModalViewable(false);
-    setSettingsModalViewable(false);
-    return true
-  }
-
-  function changePomodoroCategory(workTime: number, breakTime: number) {
-    console.log(workTime, breakTime);
-    setWorkDuration(workTime * 60);
-    setBreakDuration(breakTime * 60);
-    setTimeLeft(workTime * 60);
   }
 
     return (
         <main className='relative pt-16 md:pt-16 h-full flex flex-col items-center justify-start bg-no-repeat bg-cover bg-center' style={{backgroundImage: `url(${url})`}}>
 
                 <span className='flex rounded-md items-center w-full relative flex-col sm:flex-row justify-center gap-4 mb-4 mt-2'>
-                {!modalViewable && !settingsModalViewable && <h2 className='font-poppins absolute top-0 left-8 text-slate-800 font-semibold bg-white px-4 py-2 rounded-md shadow-lg text-xl'>Rounds: <span className='text-green-500 text-xl'>{rounds}</span></h2> }
-                    {!modalViewable &&  <nav className='flex flex-row items-center justify-center gap-0'>
-                      {workDuration == (45 * 60) ? (
+                  <h2 className='font-poppins absolute top-0 left-8 text-slate-800 font-semibold bg-white px-4 py-2 rounded-md shadow-lg text-xl'>Rounds: <span className='text-green-500 text-xl'>{rounds}</span></h2> 
+                    <nav className='flex flex-row items-center justify-center gap-0'>
+                      {duration.work == (45 * 60) ? (
                         <>
-                        <button disabled={modalViewable} className='shadow-lg border-2 rounded-md rounded-tr-none rounded-br-none px-4 py-2 text-white bg-sky-700 border-sky-700'>45/15</button> 
+                        <button className='shadow-lg border-2 rounded-md rounded-tr-none rounded-br-none px-4 py-2 text-white bg-sky-700 border-sky-700'>45/15</button> 
                         </>
                         ) : (
                       <>
-                      <button disabled={modalViewable} className='shadow-lg border-2 rounded-md rounded-tr-none rounded-br-none px-4 py-2 text-sky-700 drop-shadow-2xl bg-white border-sky-700' onClick={() => changePomodoroCategory(45,15)}>45/15</button>                     
+                      <button className='shadow-lg border-2 rounded-md rounded-tr-none rounded-br-none px-4 py-2 text-sky-700 drop-shadow-2xl bg-white border-sky-700' onClick={() => changePomodoroCategory(45,15)}>45/15</button>                     
                       </>
                       )}
-                      {workDuration == (50 * 60) ? (
-                        <button disabled={modalViewable} className='shadow-lg border-2 rounded-md rounded-tl-none rounded-bl-none px-4 py-2 text-white border-sky-700 bg-sky-700 '>50/10</button> 
+                      {duration.work == (50 * 60) ? (
+                        <button className='shadow-lg border-2 rounded-md rounded-tl-none rounded-bl-none px-4 py-2 text-white border-sky-700 bg-sky-700 '>50/10</button> 
                       ) : (
-                        <button disabled={modalViewable} className='shadow-lg border-2 rounded-md rounded-tl-none rounded-bl-none px-4 py-2 text-sky-700 bg-white drop-shadow-2xl border-sky-700' onClick={() => changePomodoroCategory(50,10)}>50/10</button>
+                        <button className='shadow-lg border-2 rounded-md rounded-tl-none rounded-bl-none px-4 py-2 text-sky-700 bg-white drop-shadow-2xl border-sky-700' onClick={() => changePomodoroCategory(50,10)}>50/10</button>
                       )}
-                      {/* {(workDuration != (45 * 60) && workDuration != (50 * 60) || settingsModalViewable) ? (
-                        <button  disabled={modalViewable} onClick={() => setSettingsModalViewable((old) => !old)} className=' ml-2 border-2 rounded-md px-4 py-2 text-slate-200 border-purple-500 bg-purple-500 hover:bg-transparent hover:text-purple-500'> <Settings size={21} /> </button> 
-                      ) : (
-                        <button disabled={modalViewable} onClick={() => setSettingsModalViewable((old) => !old)} className='ml-2 border-2 rounded-md px-4 py-2 text-purple-500 drop-shadow-2xl border-purple-500 hover:bg-purple-500 hover:text-slate-200'> <Settings size={21} /> </button>
-                      )} */}
-                    </nav>}
+
+                    </nav>
                 </span>
-                {!settingsModalViewable && (  
                 <section className='h-full flex flex-col items-center justify-center gap-8'>
                     <main className='flex flex-col items-center justify-center gap-6'>
-                          { !modalViewable && (
-                            <>
+                      
                           <p className='shadow-outline relative z-10 -mt-6 text-9xl bg-transparent text-stroke-black drop-shadow-md text-zinc-100 flex-col items-center justify-center w-80 rounded-full'>
-                            {/* {isWorking ? 'Work Time' : 'Break Time'}: */} 
                             <span className='relative z-10 flex flex-col items-center justify-center gap-0'>
                                {Math.floor(timeLeft / 60).toString().padStart(2, '0')}:{(timeLeft % 60).toString().padStart(2, '0')}
-                               <span style={{ width: 2 * percentage + 'px' }} className="h-4 bg-purple-500 rounded-md"></span>
                             </span>
-                            {/* {(timeLeft != workDuration || rounds != 0 ) && <button className=' px-3 py-1 rounded-md shadow-md drop-shadow-2xl text-lg absolute bottom-6 left-[50%] -translate-x-[50%] text-slate-200 bg-green-500' onClick={finish}>Finish</button>} */}
-                           </p>
+                          </p>
                           <span className='flex items-center justify-center gap-2'>
                           {isRunning && <button className='px-6 py-2 rounded-md shadow-md bg-slate-700 text-white hover:drop-shadow-xl' onClick={() => {resetTimer(); startTimer()}}><SkipBack /></button>}  
-
-                            <button onClick={isRunning ? pauseTimer : startTimer} className={isRunning ?' bg-green-500 text-white px-6 py-2 rounded-md shadow-md'  : ' bg-green-500 text-zinc-200 px-6 py-2 rounded-md shadow-md'}> 
+                          <button onClick={isRunning ? pauseTimer : startTimer} className={isRunning ?' bg-green-500 text-white px-6 py-2 rounded-md shadow-md'  : ' bg-green-500 text-zinc-200 px-6 py-2 rounded-md shadow-md'}> 
                               {isRunning ? <Pause /> 
                                : <Play />}
-                            </button>
+                          </button>
                                {isRunning && <button className='px-6 py-2 rounded-md shadow-md  bg-sky-700 text-white hover:drop-shadow-xl' onClick={jumpRound}><SkipForward /></button>}
                           </span>
-                          </>
-                          ) }
+                         
+                     
                       </main>
                 </section>
-                )}
         </main>
     )}
 
