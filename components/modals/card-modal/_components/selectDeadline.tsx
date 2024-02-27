24615edaad6e40da0ee4de2bcafdc31b76d4f3b2
MicaelTargino <micaelgomestargino@gmail.com>
2024-02-26 22:06:21 -0300

    refactor: deadline to be another component

---
+++ components/modals/card-modal/_components/selectDeadline.tsx
@@ -0,0 +1,43 @@
+"use client";
+import React from "react";
+import { Calendar } from "@/components/ui/calendar";
+import { Switch } from "@/components/ui/switch";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import { CalendarClock, User } from "lucide-react";
+
+
+export const SelectDeadline = () => {
+    const [date, setDate] = React.useState<Date | undefined>(new Date())
+
+    React.useEffect(()=> {
+      console.log(date)
+    }, [date])
+    
+    return (
+        <div className="w-full flex flex-col items-start justify-start gap-1">
+            <Accordion type="single" collapsible>
+             <AccordionItem value="item-1">
+                <AccordionTrigger className="hover:no-underline">
+                <p className="flex items-center justify-start gap-1 font-semibold text-neutral-700">
+                <CalendarClock className="w-5 h-5 text-neutral-700" />
+                <span className="mr-2">Deadline</span> 
+                </p>
+                </AccordionTrigger>
+                <AccordionContent>
+                    <Calendar
+                    mode="single"
+                    selected={date}
+                    onSelect={setDate}
+                    className="rounded-md border shadow"
+                    />
+                </AccordionContent>
+             </AccordionItem>
+            </Accordion>
+        </div>
+    )
+}
--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -12,19 +12,11 @@ import { Dialog, DialogContent } from "@/components/ui/dialog";
 import { Header } from "./header";
 import { Description } from "./description";
 import { Actions } from "./actions";
-import { CalendarClock, User } from "lucide-react";
 
 import * as React from "react"
 
 import { AssignUser } from "./assignuser";
-import { Calendar } from "@/components/ui/calendar";
-import { Switch } from "@/components/ui/switch";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion"
+import { SelectDeadline } from "./_components/selectDeadline";
 
 
 export const CardModal = () => {
@@ -32,11 +24,6 @@ export const CardModal = () => {
   const isOpen = useCardModal((state) => state.isOpen);
   const onClose = useCardModal((state) => state.onClose);
 
-  const [date, setDate] = React.useState<Date | undefined>(new Date())
-
-  React.useEffect(()=> {
-    console.log(date)
-  }, [date])
 
   const { data: cardData } = useQuery<CardWithList>({
     queryKey: ["card", id],
@@ -83,26 +70,7 @@ export const CardModal = () => {
           {!cardData
           ? <AssignUser.Skeleton />
           : 
-          <div className="w-full flex flex-col items-start justify-start gap-1">
-                <Accordion type="single" collapsible>
-                <AccordionItem value="item-1">
-                  <AccordionTrigger className="hover:no-underline">
-                  <p className="flex items-center justify-start gap-1 font-semibold text-neutral-700">
-                  <CalendarClock className="w-5 h-5 text-neutral-700" />
-                  <span className="mr-2">Deadline</span> 
-                </p>
-                  </AccordionTrigger>
-                  <AccordionContent>
-                    <Calendar
-                      mode="single"
-                      selected={date}
-                      onSelect={setDate}
-                      className="rounded-md border shadow"
-                    />
-                  </AccordionContent>
-                </AccordionItem>
-              </Accordion>
-          </div>
+            <SelectDeadline />
           }
           </>
         )}
